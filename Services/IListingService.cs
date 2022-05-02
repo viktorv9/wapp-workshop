43d28f07b0fccc37c4cd22087bba8bffcfbb16d9
Yael Bakker <yv.bakker@student.han.nl>
2022-05-02 11:18:07 +0200

    Add ListingService and interface

---
+++ Services/IListingService.cs
@@ -0,0 +1,13 @@
+using wapp_workshop.Models;
+
+namespace wapp_workshop.Services;
+
+public interface IListingService
+{
+    Task<IEnumerable<Listing>> GetAll();
+    Task<Listing> Get(int id);
+    Task<Listing> Update(int id, Listing listing);
+    Task<Listing> Create(Listing listing);
+    Task Delete(int id);
+    Task<bool> Exists(int id);
+}
